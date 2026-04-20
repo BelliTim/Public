bc0dcb517073426b302815fbebd704ee318df36d
BelliTim <host@tbellinghausen.de>
2026-04-20 11:15:37 +0200

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   }
 
   const response = await fetch(
-    `https://api.github.com/repos/BelliTim/BelliTim_REPO/contents/images/${filename}`,
+    `https://api.github.com/repos/BelliTim/Public_REPO/contents/images/${filename}`,
     {
       method: "PUT",
       headers: {
