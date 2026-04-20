f02af33025ff4e1e61ef9e10cc4f55528da0c6f5
BelliTim <host@tbellinghausen.de>
2026-04-21 00:01:59 +0200

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,18 +1,5 @@
 export default function handler(req, res) {
-
-    if (req.method !== "POST") {
-        return res.status(405).json({ message: "Nur POST erlaubt" });
-    }
-
-    const { password } = req.body;
-
-    if (!password) {
-        return res.status(400).json({ message: "Kein Passwort" });
-    }
-
-    if (password === process.env.ADMIN_PASSWORD) {
-        return res.status(200).json({ message: "OK" });
-    } else {
-        return res.status(401).json({ message: "Falsches Passwort" });
-    }
+    return res.status(200).json({
+        env: process.env.ADMIN_PASSWORD || "NICHT GESETZT"
+    });
 }
