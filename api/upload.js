b42a3ecf2b19672e522a54f57fa4ffd70eb246c2
BelliTim <host@tbellinghausen.de>
2026-04-21 21:26:37 +0200

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -19,6 +19,7 @@ export default async function handler(req, res) {
         spiele: "spiele.json",
         garageAktuell: "garageAktuell.json",
         garageAlt: "garageAlt.json"
+        blog: "blog.json"
     };
 
     const fileName = fileMap[category];
